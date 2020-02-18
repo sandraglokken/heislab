f92a89618053a8d963465c76df040daa3dd93639
S. Anntid <Sanntid@Sanntid.std>
2020-02-18 15:37:54 +0100

    createt .c file

---
+++ source/movement.c
@@ -0,0 +1,65 @@
+
+#include "hardware.h"
+#include <stdlib.h>
+#include "queue_system.h"
+#include "movement.h"
+
+int check_movement_conditions(Elevator elevator){
+	if (harware_read_stop_signal() || elevator.door){
+		return 0;
+	}
+	return 1;
+}
+
+
+int get_next_floor(Orders orders){
+	if (orders.next_floor< 0 || orders.next_floor>= HARDWARE_NUMBER_OF_FLOORS){
+		hardware_command_movement(HARDWARE_MOVEMENT_STOP);
+		return HARDWARE_NUMBER_OF_FLOORS;
+	}
+	return orders.next_floor;
+}
+
+
+
+void set_door(Elevator elevator,int door_open){
+	elevator.door =door_open;
+	hardware_command_door_open(door_open);
+}
+
+void set_direction_bit(Elevator elevator, int direction){
+	elevator.direction_bit=direction;
+
+
+}
+
+void set_current_floor(Elevator elevator){
+	for (int f;f<HARDWARE_NUMBER_OF_FLOORS;f++){
+		if(hardware_read_floor_sensor(f)){
+			elevator.current_floor=f;
+		}
+	}
+}
+
+
+
+void set_movement(Elevator elevator, Orders orders){
+	if (check_movement_conditions(elevator)){
+		int diff_floors, next_floor;
+		next_floor = get_next_floor(orders);
+		diff_floors = elevator.current_floor - next_floor;
+		while (!hardware_read_floor_sensor(next_floor)){
+			if (diff_floors< 0){
+				hardware_command_movement(HARDWARE_MOVEMENT_UP);
+				set_direction_bit(elevator,2);
+			}
+			if (diff_floors > 0){
+				hardware_command_movement(HARDWARE_MOVEMENT_DOWN);
+				set_direction_bit(elevator,1);
+			}
+		}
+	}
+	hardware_command_movement(HARDWARE_MOVEMENT_STOP);
+	set_direction_bit(elevator,0);
+	set_door(elevator, 1);
+}
--- source/movement.h
+++ source/movement.h
@@ -26,9 +26,10 @@ typedef struct{
 /**
  *@brief Checks all conditions for the elevator to move
  *
+ *@param elevator To get the status of the door (open/closed)
  *@return Returns 1 if all conditions are met, 0 if not
 */
-int check_movement_conditions();
+int check_movement_conditions(Elevator elevator);
 
 
 /**
@@ -37,28 +38,44 @@ int check_movement_conditions();
  * @param orders struct from queue_system.h 
  * 
  * @return Returns the next floor the elevator will move to
+ * 
+ * @warning Returns HARDWARE_NUMBER_OF_FLOORS if there is no orders
 */
 int get_next_floor(Orders orders);
+
+
+/**
+ * @brief Changes the value of Elevator.door
+ *
+ * @param elevator Uses elevator.door to set if the door i closed or open.
+ * @param door_open Set to 1 to open the door, 0 to close. Sets the value of elevator.door
+*/
+void set_door(Elevator elevator, int door_open);
+
+/**
+* @brief Sets Elevator.direction_bit depending on wether the elevator is moving up(2), down(1) or standing still(0).
+* @param direction the direction the elevator is moving
+* @param elevator Elevator struct to set its direction bit
+*/
+
+void set_direction_bit(Elevator elevator,int direction);
+
+/**
+ * @brief Updates the current floor of the elevator
+ * 
+ * @param elevator The elevator struct to update its current floor
+ * 
+ */
+
+void set_current_floor(Elevator elevator);
 
 /**
  * @brief Sets the elevator in movement using @c hardware_command_movement and @c get_next_floor
  * 
  * @param elevator Elevator struct with the current state
+ * @param orders The order of the elevator with the next floor to go to
 */
-void set_movement(Elevator elevator);
-
-/**
- * @brief Changes the value of Elevator.door
- *
-*/
-
-void set_door();
-
-/**
-* @brief Sets Elevator.direction_bit depending on wether the elevator is moving up, down or standing still.
-*/
-
-void set_direction_bit();
+void set_movement(Elevator elevator, Orders orders);
 
 #endif
 
